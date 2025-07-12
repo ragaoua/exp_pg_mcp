adda94a6b0671a64149b8d1df8f283ec99eacaf6
Reda AGAOUA <reda.agaoua@gmail.com>
2025-07-12 17:15:44 +0200

    initial commit (and tests)

---
+++ README.md
@@ -0,0 +1,11 @@
+An MCP server providing tools for interacting with a PostgreSQL database.
+
+This is a small project for me to learn Golang.
+
+# Tools
+
+- list all roles
+- list all databases
+- list all schemas in a database
+- list all objects in a schema or a database
+- execute a SQL query
---
+++ mcpg.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+)
+
+func main() {
+	conn, err := pgx.Connect(context.Background(), "postgres://postgres@localhost:5432/postgres")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	var data string
+	err = conn.QueryRow(context.Background(), "SELECT d FROM toto").Scan(&data)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(data)
+}
